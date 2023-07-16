be813f73ba4bd3c2d746cf2afc3e4bcbab301e34
Moon Quddus <moonquddus@gmail.com>
2023-07-16 17:53:26 +0100

    Add userscript file

---
+++ polished-reddit.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Polished Reddit
+// @namespace    https://github.com/moonquddus/
+// @version      0.1
+// @description  Adds mobile responsiveness 
+// @author       Moon Quddus
+// @match        *://old.reddit.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+})
